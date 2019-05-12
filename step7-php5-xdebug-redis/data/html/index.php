25e91f74c9449552a8d2463eea7a4b12016932fc
Shigehiro IDANI <g.idani@gmail.com>
2019-05-12 23:04:12 +0900

    step7の確認コードとデバッグ環境を追加

--- step7-php5-xdebug-redis/data/html/index.php
+++ step7-php5-xdebug-redis/data/html/index.php
@@ -37,7 +37,27 @@
         $redis->connect($_ENV['REDIS_HOST'], $_ENV['REDIS_PORT']);
         $redis->auth($_ENV['REDIS_PASSWORD']);
         echo '<pre class="log">';
-        echo $redis->ping();
+        echo 'Redis接続' . PHP_EOL;
+        echo $redis->ping() . PHP_EOL;
+
+        // string_keyというkeyにhugaという値をセット
+        $redis->set('string_key', 'huga');
+        // 値を取得する
+        $value = $redis->get('string_key');
+        // 表示
+        echo $value . PHP_EOL; // huga
+
+        // lPushは先頭、rPushは末尾に値をpush
+        $redis->rPush('list_key', 'a');
+        $redis->rPush('list_key', 'b');
+        $redis->lPush('list_key', 'c');
+        $redis->lPush('list_key', 'd');
+
+        // 値をすべて取得する -1はすべて
+        $value = $redis->lRange('list_key', 0, -1);
+
+        // 表示
+        var_dump($value);
         echo '</pre>';
 
         phpinfo();
