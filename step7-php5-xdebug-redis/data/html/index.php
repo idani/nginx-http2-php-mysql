90ef67d904fcc3876e742a00cddd375abf3a13f5
Shigehiro IDANI <g.idani@gmail.com>
2019-05-12 17:14:15 +0900

    REDISサンプルを追加

--- step7-php5-xdebug-redis/data/html/index.php
+++ step7-php5-xdebug-redis/data/html/index.php
@@ -2,12 +2,12 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Step.6 nginx + PHP5 + MySQL</title>
+    <title>Step.7 nginx + PHP5 + MySQL + xdebug + redis</title>
     <link rel="stylesheet" href="/main.css">
 </head>
 <body>
     <div class="container">
-        <h1 class="title">nginx + PHP5 + MySQL + xdebug</h1>
+        <h1 class="title">nginx + PHP5 + MySQL + xdebug + redis</h1>
         <img src="/img.jpg" alt="" class="thumbnail" />
 
         <?php
@@ -32,6 +32,13 @@
         echo '</pre>';
 
         mysqli_close($mysql);
+
+        $redis = new Redis();
+        $redis->connect($_ENV['REDIS_HOST'], $_ENV['REDIS_PORT']);
+        $redis->auth($_ENV['REDIS_PASSWORD']);
+        echo '<pre class="log">';
+        echo $redis->ping();
+        echo '</pre>';
 
         phpinfo();
         ?>
